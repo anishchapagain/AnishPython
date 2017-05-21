038527b6ff35816856678117e9dda4dca60dad46
anishchapagain <anishchapagain>
2017-05-21 21:33:13 +0545

    notes
    updates

--- requests_lxml_pandas/salesAnalysis.py
+++ requests_lxml_pandas/salesAnalysis.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 # Read in the csv file and display some of the basic info
-sales=pd.read_csv("sample-salesv2.csv",parse_dates=['date'])
+sales=pd.read_csv(os.path.dirname(os.path.abspath(__file__))+'/sample-salesv2.csv',parse_dates=['date'])
 print("Data types in the file:")
 print(sales.dtypes)
 print("Summary of the input file:")
