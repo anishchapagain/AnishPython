6dd57c8ba95fb2ab6d68184831790be056a155fd
Mr. Anish Chapagain <anishchapagain@users.noreply.github.com>
2020-08-30 17:52:24 +0545

    learn

---
+++ ipython_log.py
@@ -0,0 +1,16 @@
+# IPython log file
+
+dir(pd)
+get_ipython().magic(u'logstart')
+
+get_ipython().magic(u'logstart')
+import pandas as pd
+dir(pd)
+import datetime
+datetime.date.today()
+datetime.datetime.today()
+a=range(10)
+a
+a.sum()
+sum(a)
+get_ipython().magic(u'logoff')
